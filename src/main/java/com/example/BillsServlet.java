ecc57031abe111b2e0f0337fee5746fe7c08ad81
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-10-26 16:35:18 +1100

    added bills page

--- src/main/java/com/example/BillsServlet.java
+++ src/main/java/com/example/BillsServlet.java
@@ -32,7 +32,7 @@ private static final long serialVersionUID = 1L;
 				}
 			}
 			
-			String url="bills/index.jsp";
+			String url="/bills/index.jsp";
 		    ServletContext sc = getServletContext();
 		    RequestDispatcher rd = sc.getRequestDispatcher(url);
 
