92aff0c5ac259bfabe10b8f8360fda169f1f1311
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-10-01 23:45:44 +1000

    Update config to multi-tenant

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,31 +9,65 @@ import com.maestrano.Maestrano;
 
 /**
  * 
- * This class launches the web application in an embedded Jetty container.
- * This is the entry point to your application. The Java command that is used for
+ * This class launches the web application in an embedded Jetty container. This is the entry point to your application. The Java command that is used for
  * launching should fire this main method.
- *
+ * 
  */
 public class Main {
-    
+
     /**
      * @param args
      */
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         String webappDirLocation = "src/main/webapp/";
-        
+
         // Configure Maestrano API
         Properties props = new Properties();
+
+        // Environment configuration (Maestrano UAT environment)
         props.setProperty("app.environment", "test");
+        props.setProperty("app.host", "http://localhost:8080");
+//        props.setProperty("api.connecHost", "http://api-connec.uat.maestrano.io");
+//        props.setProperty("api.accountHost", "https://uat.maestrano.io");
+//        props.setProperty("sso.idp", "https://uat.maestrano.io");
+
+        // Authentication
         props.setProperty("api.id", "app-1");
-		props.setProperty("api.key", "gfcmbu8269wyi0hjazk4t7o1sndpvrqxl53e1");
-		props.setProperty("app.host", "http://java-demoapp.maestrano.io");
-		Maestrano.configure(props);
-		
-        //The port that we should run on can be set into an environment variable
-        //Look for that variable and default to 8080 if it isn't there.
+        props.setProperty("api.key", "gfcmbu8269wyi0hjazk4t7o1sndpvrqxl53e1");
+
+        // Add Connec! webhook notification path for your application
+        // Subscribe to certain entities (to receive updates from Connec!)
+        // These settings will automatically appear in your Metadata endpoint (see maestrano-java README)
+        props.setProperty("webhook.connec.notificationsPath", "/maestrano/connec/notifications");
+        props.setProperty("webhook.connec.subscriptions.company", "true");
+        props.setProperty("webhook.connec.subscriptions.organizations", "true");
+        props.setProperty("webhook.connec.subscriptions.people", "true");
+
+        Maestrano.configure("default", props);
+        
+        // For multi-tenant sintegration, define different presets
+        Properties otherProps = new Properties();
+        otherProps.setProperty("app.environment", "test");
+        otherProps.setProperty("app.host", "http://localhost:8080");
+//        otherProps.setProperty("api.connecHost", "http://api-connec.uat.maestrano.io");
+//        otherProps.setProperty("api.accountHost", "https://uat.maestrano.io");
+//        otherProps.setProperty("sso.idp", "https://uat.maestrano.io");
+        otherProps.setProperty("sso.initPath", "/maestrano/auth/saml/init/other-tenant");
+
+        otherProps.setProperty("api.id", "app-2");
+        otherProps.setProperty("api.key", "przbnuxvogf6th879kl30wi5ycje2sad41mq2");
+
+        otherProps.setProperty("webhook.connec.notificationsPath", "/maestrano/connec/notifications");
+        otherProps.setProperty("webhook.connec.subscriptions.company", "true");
+        otherProps.setProperty("webhook.connec.subscriptions.organizations", "true");
+        otherProps.setProperty("webhook.connec.subscriptions.people", "true");
+
+        Maestrano.configure("other-tenant", otherProps);
+
+        // The port that we should run on can be set into an environment variable
+        // Look for that variable and default to 8080 if it isn't there.
         String webPort = System.getenv("PORT");
-        if(webPort == null || webPort.isEmpty()) {
+        if (webPort == null || webPort.isEmpty()) {
             webPort = "8080";
         }
 
@@ -41,20 +75,20 @@ public class Main {
         WebAppContext root = new WebAppContext();
 
         root.setContextPath("/");
-        root.setDescriptor(webappDirLocation+"/WEB-INF/web.xml");
+        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
         root.setResourceBase(webappDirLocation);
-        
-        //Parent loader priority is a class loader setting that Jetty accepts.
-        //By default Jetty will behave like most web containers in that it will
-        //allow your application to replace non-server libraries that are part of the
-        //container. Setting parent loader priority to true changes this behavior.
-        //Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+
+        // Parent loader priority is a class loader setting that Jetty accepts.
+        // By default Jetty will behave like most web containers in that it will
+        // allow your application to replace non-server libraries that are part of the
+        // container. Setting parent loader priority to true changes this behavior.
+        // Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
         root.setParentLoaderPriority(true);
-        
+
         server.setHandler(root);
-        
+
         server.start();
-        server.join();   
+        server.join();
     }
 
 }
--- src/main/java/com/example/maestrano/SamlSsoServlet.java
+++ src/main/java/com/example/maestrano/SamlSsoServlet.java
@@ -1,6 +1,8 @@
 package com.example.maestrano;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -11,77 +13,89 @@ import javax.servlet.http.HttpSession;
 
 import com.maestrano.saml.AuthRequest;
 import com.maestrano.saml.Response;
-import com.maestrano.sso.*;
+import com.maestrano.sso.MnoGroup;
+import com.maestrano.sso.MnoSession;
+import com.maestrano.sso.MnoUser;
 
 public class SamlSsoServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		AuthRequest authReq = new AuthRequest(req);
-		try {
-			String ssoUrl = authReq.getRedirectUrl();
-			resp.sendRedirect(ssoUrl);
+    private Pattern regExTenantPattern = Pattern.compile("/maestrano/auth/saml/init/([a-zA-Z0-9\\-]*)");
 
-		} catch (Exception e) {
-			ServletOutputStream out = resp.getOutputStream();
-			e.printStackTrace();
-			out.write(e.getMessage().getBytes());
-			out.flush();
-			out.close();
-		}
-	}
+    // GET /maestrano/auth/saml/init
+    // GET /maestrano/auth/saml/init/other-tenant
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Check for ID case first, since the All pattern would also match
+        String preset = "default";
+        Matcher matcher = regExTenantPattern.matcher(req.getRequestURI());
+        if (matcher.find()) {
+            preset = matcher.group(1);
+        }
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Response authResp = null;
-		try {
-			authResp = new Response();
-			authResp.loadXmlFromBase64(req.getParameter("SAMLResponse"));
-			
-			if (authResp.isValid()) {
-				
-				// Build maestrano user and group objects
-				MnoUser mnoUser = new MnoUser(authResp);
-				MnoGroup mnoGroup = new MnoGroup(authResp);
+        AuthRequest authReq = new AuthRequest(preset, req);
+        try {
+            String ssoUrl = authReq.getRedirectUrl();
+            resp.sendRedirect(ssoUrl);
 
-				// No database model in this project. We just keep the
-				// relevant details in session
-				HttpSession sess = req.getSession();
-				sess.setAttribute("loggedIn", true);
-				sess.setAttribute("name", mnoUser.getFirstName());
-				sess.setAttribute("surname", mnoUser.getLastName());
-				sess.setAttribute("groupName", mnoGroup.getName());
-				sess.setAttribute("groupId", mnoGroup.getUid());
+        } catch (Exception e) {
+            ServletOutputStream out = resp.getOutputStream();
+            e.printStackTrace();
+            out.write(e.getMessage().getBytes());
+            out.flush();
+            out.close();
+        }
+    }
 
-				// Set Maestrano session (used for Single Logout)
-				MnoSession mnoSession = new MnoSession(req.getSession(),mnoUser);
-				mnoSession.save();
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Response authResp = null;
+        try {
+            authResp = new Response();
+            authResp.loadXmlFromBase64(req.getParameter("SAMLResponse"));
 
-				// Redirect to you application home page
-				resp.sendRedirect("/");
+            if (authResp.isValid()) {
 
-			} else {
-				ServletOutputStream out = resp.getOutputStream();
-				out.write("SAML Response is invalid".getBytes());
-				out.flush();
-				out.close();
-			}
+                // Build maestrano user and group objects
+                MnoUser mnoUser = new MnoUser(authResp);
+                MnoGroup mnoGroup = new MnoGroup(authResp);
 
-		} catch (Exception e) {
-			ServletOutputStream out = resp.getOutputStream();
-			e.printStackTrace();
-			out.write(e.getMessage().getBytes());
-			out.flush();
-			out.close();
-		}
+                // No database model in this project. We just keep the
+                // relevant details in session
+                HttpSession sess = req.getSession();
+                sess.setAttribute("loggedIn", true);
+                sess.setAttribute("name", mnoUser.getFirstName());
+                sess.setAttribute("surname", mnoUser.getLastName());
+                sess.setAttribute("groupName", mnoGroup.getName());
+                sess.setAttribute("groupId", mnoGroup.getUid());
 
-		ServletOutputStream out = resp.getOutputStream();
+                // Set Maestrano session (used for Single Logout)
+                MnoSession mnoSession = new MnoSession(req.getSession(), mnoUser);
+                mnoSession.save();
 
-		out.write("Hello Servlet!".getBytes());
-		out.flush();
-		out.close();
-	}
+                // Redirect to you application home page
+                resp.sendRedirect("/");
+
+            } else {
+                ServletOutputStream out = resp.getOutputStream();
+                out.write("SAML Response is invalid".getBytes());
+                out.flush();
+                out.close();
+            }
+
+        } catch (Exception e) {
+            ServletOutputStream out = resp.getOutputStream();
+            e.printStackTrace();
+            out.write(e.getMessage().getBytes());
+            out.flush();
+            out.close();
+        }
+
+        ServletOutputStream out = resp.getOutputStream();
+
+        out.write("Hello Servlet!".getBytes());
+        out.flush();
+        out.close();
+    }
 
 }
-
