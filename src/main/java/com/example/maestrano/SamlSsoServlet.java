ed2b05cb9c371c18f27aee81739a6b1c8ee8c9e4
x4d3 <git@xade.eu>
2016-07-15 15:51:09 +0100

    Use embeded tomcat instead of jetty
    fix #8

--- src/main/java/com/example/maestrano/SamlSsoServlet.java
+++ src/main/java/com/example/maestrano/SamlSsoServlet.java
@@ -6,13 +6,13 @@ import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.maestrano.Maestrano;
-import com.maestrano.exception.MnoConfigurationException;
 import com.maestrano.exception.MnoException;
 import com.maestrano.saml.AuthRequest;
 import com.maestrano.saml.Response;
@@ -20,89 +20,90 @@ import com.maestrano.sso.MnoGroup;
 import com.maestrano.sso.MnoSession;
 import com.maestrano.sso.MnoUser;
 
+@WebServlet(urlPatterns = { "/maestrano/auth/saml/init", "/maestrano/auth/saml/init/*", "/maestrano/auth/saml/consume" })
 public class SamlSsoServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private Pattern regExTenantPattern = Pattern.compile("/maestrano/auth/saml/init/([a-zA-Z0-9\\-]*)");
+	private Pattern regExTenantPattern = Pattern.compile("/maestrano/auth/saml/init/([a-zA-Z0-9\\-]*)");
 
-    // GET /maestrano/auth/saml/init
-    // GET /maestrano/auth/saml/init/other-tenant
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Check for ID case first, since the All pattern would also match
-        String preset = "default";
-        Matcher matcher = regExTenantPattern.matcher(req.getRequestURI());
-        if (matcher.find()) {
-            preset = matcher.group(1);
-        }
-        AuthRequest authReq;
+	// GET /maestrano/auth/saml/init
+	// GET /maestrano/auth/saml/init/other-tenant
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// Check for ID case first, since the All pattern would also match
+		String preset = "default";
+		Matcher matcher = regExTenantPattern.matcher(req.getRequestURI());
+		if (matcher.find()) {
+			preset = matcher.group(1);
+		}
+		AuthRequest authReq;
 		try {
 			authReq = new AuthRequest(Maestrano.get(preset), req);
 		} catch (MnoException e) {
 			throw new ServletException("Maestrano was not well configured", e);
 		}
-        try {
-            String ssoUrl = authReq.getRedirectUrl();
-            resp.sendRedirect(ssoUrl);
+		try {
+			String ssoUrl = authReq.getRedirectUrl();
+			resp.sendRedirect(ssoUrl);
 
-        } catch (Exception e) {
-            ServletOutputStream out = resp.getOutputStream();
-            e.printStackTrace();
-            out.write(e.getMessage().getBytes());
-            out.flush();
-            out.close();
-        }
-    }
+		} catch (Exception e) {
+			ServletOutputStream out = resp.getOutputStream();
+			e.printStackTrace();
+			out.write(e.getMessage().getBytes());
+			out.flush();
+			out.close();
+		}
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Response authResp = null;
-        try {
-            authResp = new Response();
-            authResp.loadXmlFromBase64(req.getParameter("SAMLResponse"));
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Response authResp = null;
+		try {
+			authResp = new Response();
+			authResp.loadXmlFromBase64(req.getParameter("SAMLResponse"));
 
-            if (authResp.isValid()) {
+			if (authResp.isValid()) {
 
-                // Build maestrano user and group objects
-                MnoUser mnoUser = new MnoUser(authResp);
-                MnoGroup mnoGroup = new MnoGroup(authResp);
+				// Build maestrano user and group objects
+				MnoUser mnoUser = new MnoUser(authResp);
+				MnoGroup mnoGroup = new MnoGroup(authResp);
 
-                // No database model in this project. We just keep the
-                // relevant details in session
-                HttpSession sess = req.getSession();
-                sess.setAttribute("loggedIn", true);
-                sess.setAttribute("name", mnoUser.getFirstName());
-                sess.setAttribute("surname", mnoUser.getLastName());
-                sess.setAttribute("groupName", mnoGroup.getName());
-                sess.setAttribute("groupId", mnoGroup.getUid());
+				// No database model in this project. We just keep the
+				// relevant details in session
+				HttpSession sess = req.getSession();
+				sess.setAttribute("loggedIn", true);
+				sess.setAttribute("name", mnoUser.getFirstName());
+				sess.setAttribute("surname", mnoUser.getLastName());
+				sess.setAttribute("groupName", mnoGroup.getName());
+				sess.setAttribute("groupId", mnoGroup.getUid());
 
-                // Set Maestrano session (used for Single Logout)
-                MnoSession mnoSession = new MnoSession(req.getSession(), mnoUser);
-                mnoSession.save();
+				// Set Maestrano session (used for Single Logout)
+				MnoSession mnoSession = new MnoSession(req.getSession(), mnoUser);
+				mnoSession.save();
 
-                // Redirect to you application home page
-                resp.sendRedirect("/");
+				// Redirect to you application home page
+				resp.sendRedirect("/");
 
-            } else {
-                ServletOutputStream out = resp.getOutputStream();
-                out.write("SAML Response is invalid".getBytes());
-                out.flush();
-                out.close();
-            }
+			} else {
+				ServletOutputStream out = resp.getOutputStream();
+				out.write("SAML Response is invalid".getBytes());
+				out.flush();
+				out.close();
+			}
 
-        } catch (Exception e) {
-            ServletOutputStream out = resp.getOutputStream();
-            e.printStackTrace();
-            out.write(e.getMessage().getBytes());
-            out.flush();
-            out.close();
-        }
+		} catch (Exception e) {
+			ServletOutputStream out = resp.getOutputStream();
+			e.printStackTrace();
+			out.write(e.getMessage().getBytes());
+			out.flush();
+			out.close();
+		}
 
-        ServletOutputStream out = resp.getOutputStream();
+		ServletOutputStream out = resp.getOutputStream();
 
-        out.write("Hello Servlet!".getBytes());
-        out.flush();
-        out.close();
-    }
+		out.write("Hello Servlet!".getBytes());
+		out.flush();
+		out.close();
+	}
 
 }
